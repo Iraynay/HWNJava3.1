ca03fab7ad81fd0aa8d478aced820a08ab210743
Ирина Пащенко <i.m.uglanova@gmail.com>
2022-05-15 13:35:37 +0300

    update1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int amount = 1000;
-        int bonus;
-        bonus = amount / 20;
-        System.out.println("Bonus=" + bonus);
-        
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
